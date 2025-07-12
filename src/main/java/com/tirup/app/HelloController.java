d89ef6fd840ef0ba41306901b1aa139ce1c0cc27
tirupdev <ulli.tirupataiah@gmail.com>
2025-07-12 08:58:41 +0530

    build-springboot-tomcat4

--- src/main/java/com/tirup/app/HelloController.java
+++ src/main/java/com/tirup/app/HelloController.java
@@ -1,18 +1,12 @@
 package com.tirup.app;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.*;
 
-@SpringBootApplication
-public class PetApp extends SpringBootServletInitializer {
-  public static void main(String[] args) {
-    SpringApplication.run(PetApp.class, args);
-  }
+@RestController
+public class HelloController {
 
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-    return builder.sources(PetApp.class);
-  }
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Spring Boot!";
+    }
 }
